45fa5c2c5aa4e50ecb9c5c14d3d5cb36ba6292cd
Ben Hudson <ben@wedo.digital>
2023-07-25 13:12:15 +0100

    Updating the config with the correct URL

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -7,8 +7,8 @@ require('dotenv').config({
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: 'ThreeSixty',
-    siteUrl: 'https://www.yourdomain.tld',
+    title: 'Threesixty',
+    siteUrl: 'https://threesixty-brands.com/',
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
